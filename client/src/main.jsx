1be6c681fb44b80fbd2dab4d9cc2aa705ef1e91f
Abinanthan <143276733+Abinanthan47@users.noreply.github.com>
2024-03-31 19:23:45 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routers/router.jsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, RouterProvider } from 'react-router-dom'; // Import BrowserRouter and RouterProvider
+import App from './App.jsx';
+import './index.css';
+import router from './routers/router.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Router> {/* Wrap your App component with the Router */}
+      <RouterProvider router={router} /> {/* Pass your router to the RouterProvider */}
+      <App />
+    </Router>
+  </React.StrictMode>
+);
